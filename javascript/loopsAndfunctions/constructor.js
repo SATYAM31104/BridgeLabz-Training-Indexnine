0cc9c70ba13e2d6399b28fb9bca9802adfd1cd76
shashwat srivastava <113302101+SATYAM31104@users.noreply.github.com>
2025-10-31 12:09:48 +0530

    few changes

--- javascript/loopsAndfunctions/constructor.js
+++ javascript/loopsAndfunctions/constructor.js
@@ -13,3 +13,61 @@ const ben = new Person("ben");
 const sam = new Person("sam");
 console.log(ben.talk());
  
+class Check{
+    constructor(name){
+        this.name = name;
+    }
+    greet () {
+        return `what are you doing ${this.name}`;
+    }
+}
+
+const OBJ = new Check("shashwat");
+console.log(OBJ.greet());
+
+const naming= new Person("shashwat");
+console.log(naming.name);
+
+//normal function 
+// function person (name){
+//     return {
+//         name : name,
+//         age : 12 ,
+//         wow(){
+//             return `is it true ${this.name}`;
+//         }
+//     }
+// }
+// const x = person("shashwat");
+// console.log(x.name);
+// console.log(x.age);
+// console.log(x.wow());
+
+//constructor function
+// function person(name,age){
+//     this.name = name ,
+//     this.age = age,
+//     this.wow = () =>{
+//         return `is this true ${this.name}`;
+//     }
+// }
+// //here we have to create and instance using the new Keyword
+// const x = new person("shashwat",21);
+// console.log(x.name);
+// console.log(x.age);
+// console.log(x.wow());
+
+//using class and calling 
+class person {
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+    wow(){
+        return `is this true ${this.name}`;
+    }
+}
+const x = new person("shashwat",21);
+console.log(x.name);
+console.log(x.age);
+console.log(x.wow());
--- javascript/loopsAndfunctions/factory.js
+++ javascript/loopsAndfunctions/factory.js
@@ -16,4 +16,21 @@ console.log(me.talk());
 const aarna = personFactory("Aarna");
 console.log(aarna.talk());
 
+function createPerson(name){
+    return obj = { //returns an obj
+        name : name,
+        talk(){
+            return `hi how are you ${this.name}`;
+        }
+    }
+}
+
+ const ME = createPerson("shashwat");
+ console.log(ME.talk());
  
+ME.talk = function() {
+    return `this is a war ${this.name}`;
+};
+ console.log(ME.talk());
+ const YOU = createPerson("Aarna");
+ console.log(YOU.talk());
