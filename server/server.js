93f96c332526aa27aa1b0127ee392759a9fc2396
shashwat srivastava <113302101+SATYAM31104@users.noreply.github.com>
2025-11-06 23:48:58 +0530

    new

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((req,res)=>{
-    res.write('Hello this is the node js server created');
-    res.end();
+    if(req.url === '/'){
+        res.end("welcome to the home page");
+    }
+    else if(req.url === '/about'){
+        res.end("This is the about page");
+    }
+    else{
+        res.writeHead(404,{'content-type':'text/plain'});
+        res.end("404 page not found");
+    }
 });
+
 
 server.listen(3000,()=>{
     console.log("Server is listening on port 3000");
