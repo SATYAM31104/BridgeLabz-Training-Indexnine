f388dc2c51990fc7604ea68c08e9d6baa1b2de42
shashwat srivastava <113302101+SATYAM31104@users.noreply.github.com>
2025-11-06 20:18:13 +0530

    new

--- server/reqres.js
+++ server/reqres.js
@@ -13,12 +13,25 @@
 // })
 
 const http = require("http");
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/plain'});
-    res.write("Hello this is the response from the server");
-    res.end();
-});
+// const server = http.createServer((req,res)=>{
+//     res.writeHead(200,{'content-type':'text/plain'});
+//     res.write("Hello this is the response from the server");
+//     res.end();
+// });
 
-server.listen(3000,()=>{
-    console.log("server is listening on the port 3000");
+// server.listen(3000,()=>{
+//     console.log("server is listening on the port 3000");
+// })
+
+const fs = require('fs');
+const myserver = http.createServer((req,res)=>{
+    const log = `${ Date.now()} : ${req.method} : ${req.url} new requests have beend recieved`;
+   fs.appendFile('requests.txt', log + '\n',(err)=>{
+        if(err) throw err;
+        res.end("the request has been logged");
+    })
+})
+
+myserver.listen(4000,()=>{
+    console.log("the app is running on the port 4000");
 })
